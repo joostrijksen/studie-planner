68c6dc84cbe7bda6891dec30ed9b3296c9f946d9
joostrijksen <joostrijksen@me.com>
2026-02-25 22:22:34 +0100

    Update BreakoutGame.tsx

--- components/BreakoutGame.tsx
+++ components/BreakoutGame.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useRef, useState } from 'react';
 
-// ─── Types ────────────────────────────────────────────────────────────────────
+// ─── 1Types ────────────────────────────────────────────────────────────────────
 
 type BreakoutGameProps = {
   onGameOver: (score: number) => void;
